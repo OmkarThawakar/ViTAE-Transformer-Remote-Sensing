31d57e1fdce2e1978ac7d71b54804176136b0760
OmkarThawakar <omee0805@gmail.com>
2022-07-18 20:26:58 +0400

    stage branch created

---
+++ Scene Recognition/local_run.sh
@@ -0,0 +1,5 @@
+# bin/bash
+
+python -m torch.distributed.launch --nproc_per_node 1 --master_port 8889 main.py \
+--dataset 'millionAID' --model 'vitae_win' --exp_num 1 --batch-size 8 --epochs 300 \
+--img_size 224 --split 100 --lr 5e-4  --weight_decay 0.05 --gpu_num 1 --output test_run3
